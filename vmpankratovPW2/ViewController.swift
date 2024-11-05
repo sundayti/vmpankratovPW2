e8e6ddd7b98f64b39a3c16aba8dd46712f34318a
sundayti <pankratovvlad1@gmail.com>
2024-11-05 13:44:08 +0300

    Initial Commit

---
+++ vmpankratovPW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vmpankratovPW2
+//
+//  Created by Tom Tim on 05.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
