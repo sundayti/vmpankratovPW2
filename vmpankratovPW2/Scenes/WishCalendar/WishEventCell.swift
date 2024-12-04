b6170b4e9099ddf3dd73f879bda24eee132f469c
sundayti <pankratovvlad1@gmail.com>
2024-12-05 02:27:07 +0300

    Remove magic numbers

--- vmpankratovPW2/Scenes/WishCalendar/WishEventCell.swift
+++ vmpankratovPW2/Scenes/WishCalendar/WishEventCell.swift
@@ -9,27 +9,40 @@ import UIKit
 
 final class WishEventCell: UICollectionViewCell {
     static let reuseIdentifier: String = "WishEventCell"
+    enum Constants {
+        static let wrapRadius: CGFloat = 16
+        static let wrapBorderWidth: CGFloat = 1
+        
+        static let titleFont: UIFont = .boldSystemFont(ofSize: 18)
+        static let descriptionFont: UIFont = .systemFont(ofSize: 14)
+        static let dateFont: UIFont = .italicSystemFont(ofSize: 12)
+        
+        static let topPadding: CGFloat = 5
+        static let leadingPadding: CGFloat = 10
+        static let trailingPadding: CGFloat = 10
+        static let bottomPadding: CGFloat = 10
+    }
     
     // MARK: - UI Elements
     private let wrapView: UIView = {
         let view = UIView()
-        view.layer.cornerRadius = 16
+        view.layer.cornerRadius = Constants.wrapRadius
         view.backgroundColor = .white
-        view.layer.borderWidth = 1
+        view.layer.borderWidth = Constants.wrapBorderWidth
         view.layer.borderColor = UIColor.black.cgColor
         return view
     }()
     
     private let titleLabel: UILabel = {
         let label = UILabel()
-        label.font = .boldSystemFont(ofSize: 18)
+        label.font = Constants.titleFont
         label.textColor = .black
         return label
     }()
     
     private let descriptionLabel: UILabel = {
         let label = UILabel()
-        label.font = .systemFont(ofSize: 14)
+        label.font = Constants.descriptionFont
         label.textColor = .darkGray
         label.numberOfLines = 0
         return label
@@ -37,14 +50,14 @@ final class WishEventCell: UICollectionViewCell {
     
     private let startDateLabel: UILabel = {
         let label = UILabel()
-        label.font = .italicSystemFont(ofSize: 12)
+        label.font = Constants.dateFont
         label.textColor = .gray
         return label
     }()
     
     private let endDateLabel: UILabel = {
         let label = UILabel()
-        label.font = .italicSystemFont(ofSize: 12)
+        label.font = Constants.dateFont
         label.textColor = .gray
         return label
     }()
@@ -66,8 +79,8 @@ final class WishEventCell: UICollectionViewCell {
     
     // MARK: - Cell Configuration
     func configure(with event: WishEvent) {
-        titleLabel.text = event.title ?? "No Title"
-        descriptionLabel.text = event.eventDescription ?? "No Description"
+        titleLabel.text = event.title ?? ""
+        descriptionLabel.text = event.eventDescription ?? ""
         
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .medium
@@ -76,51 +89,51 @@ final class WishEventCell: UICollectionViewCell {
         if let startDate = event.startDate {
             startDateLabel.text = "Start Date: \(dateFormatter.string(from: startDate))"
         } else {
-            startDateLabel.text = "Start Date: N/A"
+            startDateLabel.text = ""
         }
         
         if let endDate = event.endDate {
             endDateLabel.text = "End Date: \(dateFormatter.string(from: endDate))"
         } else {
-            endDateLabel.text = "End Date: N/A"
+            endDateLabel.text = ""
         }
     }
     
     // MARK: - UI Configuration
     private func configureWrap() {
         contentView.addSubview(wrapView)
-        wrapView.pinTop(to: contentView.topAnchor, 5)
-        wrapView.pinBottom(to: contentView.bottomAnchor, 5)
-        wrapView.pinLeft(to: contentView.leadingAnchor, 10)
-        wrapView.pinRight(to: contentView.trailingAnchor, 10)
+        wrapView.pinTop(to: contentView.topAnchor, Constants.topPadding)
+        wrapView.pinBottom(to: contentView.bottomAnchor, Constants.bottomPadding)
+        wrapView.pinLeft(to: contentView.leadingAnchor, Constants.leadingPadding)
+        wrapView.pinRight(to: contentView.trailingAnchor, Constants.trailingPadding)
     }
     
     private func configureTitleLabel() {
         wrapView.addSubview(titleLabel)
-        titleLabel.pinTop(to: wrapView.topAnchor, 5)
-        titleLabel.pinLeft(to: wrapView.leadingAnchor, 10)
-        titleLabel.pinRight(to: wrapView.trailingAnchor, 10)
+        titleLabel.pinTop(to: wrapView.topAnchor,  Constants.topPadding)
+        titleLabel.pinLeft(to: wrapView.leadingAnchor, Constants.leadingPadding)
+        titleLabel.pinRight(to: wrapView.trailingAnchor, Constants.trailingPadding)
     }
     
     private func configureDescriptionLabel() {
         wrapView.addSubview(descriptionLabel)
-        descriptionLabel.pinTop(to: titleLabel.bottomAnchor, 5)
-        descriptionLabel.pinLeft(to: wrapView.leadingAnchor, 10)
-        descriptionLabel.pinRight(to: wrapView.trailingAnchor, 10)
+        descriptionLabel.pinTop(to: titleLabel.bottomAnchor,  Constants.topPadding)
+        descriptionLabel.pinLeft(to: wrapView.leadingAnchor, Constants.leadingPadding)
+        descriptionLabel.pinRight(to: wrapView.trailingAnchor, Constants.trailingPadding)
     }
     
     private func configureStartDateLabel() {
         wrapView.addSubview(startDateLabel)
-        startDateLabel.pinTop(to: descriptionLabel.bottomAnchor, 5)
-        startDateLabel.pinLeft(to: wrapView.leadingAnchor, 10)
-        startDateLabel.pinRight(to: wrapView.trailingAnchor, 10)
+        startDateLabel.pinTop(to: descriptionLabel.bottomAnchor,  Constants.topPadding)
+        startDateLabel.pinLeft(to: wrapView.leadingAnchor, Constants.leadingPadding)
+        startDateLabel.pinRight(to: wrapView.trailingAnchor, Constants.trailingPadding)
     }
     
     private func configureEndDateLabel() {
         wrapView.addSubview(endDateLabel)
-        endDateLabel.pinTop(to: startDateLabel.bottomAnchor, 5)
-        endDateLabel.pinLeft(to: wrapView.leadingAnchor, 10)
-        endDateLabel.pinRight(to: wrapView.trailingAnchor, 10)
-        endDateLabel.pinBottom(to: wrapView.bottomAnchor, 10)
+        endDateLabel.pinTop(to: startDateLabel.bottomAnchor,  Constants.topPadding)
+        endDateLabel.pinLeft(to: wrapView.leadingAnchor, Constants.leadingPadding)
+        endDateLabel.pinRight(to: wrapView.trailingAnchor, Constants.trailingPadding)
+        endDateLabel.pinBottom(to: wrapView.bottomAnchor, Constants.bottomPadding)
     }
 }
