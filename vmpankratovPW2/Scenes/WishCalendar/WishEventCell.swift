d60d294661aafa7b3acbdff96c48bcd7077ddfab
sundayti <pankratovvlad1@gmail.com>
2024-12-05 00:38:22 +0300

    Create Properties in WishEventCell

--- vmpankratovPW2/Scenes/WishCalendar/WishEventCell.swift
+++ vmpankratovPW2/Scenes/WishCalendar/WishEventCell.swift
@@ -8,5 +8,46 @@
 import UIKit
 
 final class WishEventCell: UICollectionViewCell {
+    static let reuseIdentifier: String = "WishEventCell"
+    
+    // MARK: - UI Elements
+    private let wrapView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 16
+        view.backgroundColor = .white
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.lightGray.cgColor
+        return view
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .darkGray
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let startDateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .italicSystemFont(ofSize: 12)
+        label.textColor = .gray
+        return label
+    }()
+    
+    private let endDateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .italicSystemFont(ofSize: 12)
+        label.textColor = .gray
+        return label
+    }()
+    
 
 }
