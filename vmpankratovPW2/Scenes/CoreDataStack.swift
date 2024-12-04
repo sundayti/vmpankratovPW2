bc14ca4b0b21b15bc9f85b191dbaa55d37692dcb
sundayti <pankratovvlad1@gmail.com>
2024-12-05 01:24:39 +0300

    Move CoreDataStack

---
+++ vmpankratovPW2/Scenes/CoreDataStack.swift
@@ -0,0 +1,41 @@
+//
+//  ShowDataStack.swift
+//  vmpankratovPW2
+//
+//  Created by Tom Tim on 06.11.2024.
+//
+
+import CoreData
+import UIKit
+
+final class CoreDataStack {
+    static let shared = CoreDataStack()
+    
+    private init() {}
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "WishModel")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
